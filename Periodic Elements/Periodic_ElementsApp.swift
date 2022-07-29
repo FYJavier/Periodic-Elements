3c60bcfea02c4d51857b862054ec60b2c2e1bae3
Freya Javier <thesleepyunicorn63@gmail.com>
2022-07-29 10:19:34 -0500

    Initial Commit

---
+++ Periodic Elements/Periodic_ElementsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Periodic_ElementsApp.swift
+//  Periodic Elements
+//
+//  Created by Yuna on 7/29/22.
+//
+
+import SwiftUI
+
+@main
+struct Periodic_ElementsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
